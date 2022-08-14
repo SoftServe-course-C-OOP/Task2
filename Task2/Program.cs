7e738c7eaa4e622a10472fd911589b348a6589f4
Maksym Semenov <mestcomua@gmail.com>
2022-08-14 14:00:06 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What does eat your cat?");
+            Cat cat = new Cat();
+            cat.Meal(Food.Meat);
+            Console.WriteLine($"Your cat's food level is {cat.FoodLevel}");
+            Console.WriteLine();
+        }
+        class Cat
+        {
+            public int FoodLevel { get; set; } 
+            public int Meal(Food food)
+            {
+                return FoodLevel += (int) food;
+            }
+        }
+        enum Food
+        {
+            Milk = 10,
+            Mouse = 20,
+            Chiken = 30,
+            Meat = 40
+        }
+    }
+}
